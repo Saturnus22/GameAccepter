50a6ec4ac0b2ba721f69d7f8a3e26017cdb92eb4
Saturnus22 <69690366+Saturnus22@users.noreply.github.com>
2021-09-02 01:07:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pyautogui
 
 while True:
     time.sleep(1)
-    loc = pyautogui.locateOnScreen('img.png',confidence=.5)
+    loc = pyautogui.locateOnScreen('imgs/img.png',confidence=.5)
     if loc:
         time.sleep(3)
         pyautogui.click(loc)
